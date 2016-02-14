65e0ff0856faa89227a88569ac5fc47e76781df2
Nazar Kisil <nazarkisil91.nk@google.com>
2016-02-14 06:02:21 +0200

    date and time

--- app/src/main/java/ua/te/hackathon/smartcity2015/ui/main/EventCreationActivity.java
+++ app/src/main/java/ua/te/hackathon/smartcity2015/ui/main/EventCreationActivity.java
@@ -10,17 +10,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
 import ua.te.hackathon.smartcity2015.R;
 import ua.te.hackathon.smartcity2015.model.Event;
-import ua.te.hackathon.smartcity2015.utils.Utils;
 
 import static ua.te.hackathon.smartcity2015.utils.Utils.date;
 import static ua.te.hackathon.smartcity2015.utils.Utils.text;
 
-public class EventCreationActivity extends AppCompatActivity {
+public class EventCreationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     @Bind(R.id.etInputDate)
     EditText etInputDate;
@@ -33,12 +41,20 @@ public class EventCreationActivity extends AppCompatActivity {
 
     @Bind(R.id.etDescription)
     EditText etDescription;
+    private long date;
+    private long time;
+
+    public static Intent startActivity(Context applicationContext) {
+        Intent intent = new Intent(applicationContext, EventCreationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ButterKnife.bind(this);
         setContentView(R.layout.activity_event_creation);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -53,7 +69,7 @@ public class EventCreationActivity extends AppCompatActivity {
                                 // TODO: 14.02.16 add background image url
                                 Event event = new Event();
                                 event.setName(text(etName));
-                                event.setDate(date(text(etInputDate)));
+                                event.setDate(date + time);
                                 event.setDescription(text(etDescription));
                                 event.setPlace(text(etPlace));
 
@@ -69,9 +85,50 @@ public class EventCreationActivity extends AppCompatActivity {
         });
     }
 
-    public static Intent startActivity(Context applicationContext) {
-        Intent intent = new Intent(applicationContext, EventCreationActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        return intent;
+    @OnClick({R.id.btPickDate, R.id.btPickTime})
+    public void choosDateTime(View button){
+        Calendar now = Calendar.getInstance();
+        switch (button.getId()){
+            case R.id.btPickDate:
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+                break;
+            case R.id.btPickTime:
+                TimePickerDialog tpd = TimePickerDialog.newInstance(
+                        this,
+                        now.get(Calendar.HOUR),
+                        now.get(Calendar.MINUTE),
+                        true
+                );
+                tpd.show(getFragmentManager(), "Timepickerdialog");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(year, monthOfYear, dayOfMonth);
+        Date dat = date.getTime();
+        this.date = dat.getTime();
+        date.setTimeInMillis(this.date + this.time);
+        etInputDate.setText(dat.toString());
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+        Calendar date = GregorianCalendar.getInstance();
+
+        Date dat = date.getTime();
+        this.time = dat.getTime();
+        date.setTimeInMillis(this.date + this.time);
+        etInputDate.setText(dat.toString());
+
     }
 }
